753c5eb2c32873fbffc13fff2c9827f6ec7e94cf
tkla <tkla@ucsc.edu>
2018-12-04 23:18:20 -0800

    Removed printing stack

--- pair.java
+++ pair.java
@@ -24,9 +24,6 @@ public class pair{
                }
             }
          }
-         System.out.println(Result);
-         
-         
       }
       
       catch ( FileNotFoundException fnfe){
