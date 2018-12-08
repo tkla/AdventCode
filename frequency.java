f0c44858de73ac3f3dd32dead382e304a74b3f37
Kenny <kennyla95@gmail.com>
2018-12-08 22:58:47 +0000

    Part 2 completed with hash map

--- frequency.java
+++ frequency.java
@@ -20,11 +20,37 @@ public class frequency{
         return count;   
     }
     
-    
+    public static int FirstRepeat(String filename){
+        HashMap<Integer, Integer> frequency = new HashMap<>();
+        int count = 0;
+        while(true){
+            try {
+                BufferedReader in = new BufferedReader(new FileReader (filename));
+                String number;
+                while ((number = in.readLine()) != null){
+                    count += Integer.parseInt(number);
+                    if (frequency.containsKey(count)) {
+                        return frequency.get(count);
+                    }
+                    else{
+                        frequency.put(count, count);
+                    }
+                }
+            }
+            catch(FileNotFoundException fnfe){
+                System.out.println(fnfe);
+            }
+            catch(IOException ioexcept){
+                 System.out.println(ioexcept);
+            }
+        }
+
+    }
     
     
     public static void  main(String[] args){
         System.out.println(count("numbers"));
+        System.out.println(FirstRepeat("numbers"));
         
     }
 }
